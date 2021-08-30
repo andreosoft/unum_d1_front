323322420279707845072739aab57293ac0a78f5
andreosoft <andreyzagorets@gmail.com>
2021-08-30 13:59:17 +0300

    change http to https

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const localStorage = JSON.parse(window.localStorage.getItem('neomedy')) || ''
 
-axios.defaults.baseURL = 'http://api.neomedy.com/api'
+axios.defaults.baseURL = 'https://api.neomedy.com/api'
 axios.defaults.headers.common['Authorization'] = localStorage.token
 
 export default axios
