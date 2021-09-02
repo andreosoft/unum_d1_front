c0382eb14745ad7c17bc1a7971905cf0a8cd8f44
Emir <dzhetybaev19emir@gmail.com>
2021-09-02 14:45:52 +0600

    portal-vue

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import Vuelidate from 'vuelidate'
-import VuePhoneNumberInput from 'vue-phone-number-input';
-import 'vue-phone-number-input/dist/vue-phone-number-input.css';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import Vuelidate from "vuelidate";
+import VuePhoneNumberInput from "vue-phone-number-input";
+import PortalVue from "portal-vue";
+import "vue-phone-number-input/dist/vue-phone-number-input.css";
 
-Vue.use(Vuelidate)
-Vue.config.productionTip = false
-Vue.component('vue-phone-number-input', VuePhoneNumberInput);
+Vue.use(PortalVue);
+Vue.use(Vuelidate);
+Vue.config.productionTip = false;
+Vue.component("vue-phone-number-input", VuePhoneNumberInput);
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
