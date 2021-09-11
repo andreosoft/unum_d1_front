e662858ef99720fd9fced8dceaf36c711998ef68
andreosoft <andreyzagorets@gmail.com>
2021-09-11 16:11:26 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import Vuelidate from 'vuelidate'
-import VuePhoneNumberInput from 'vue-phone-number-input';
-import 'vue-phone-number-input/dist/vue-phone-number-input.css';
-import './registerServiceWorker'
-=======
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -18,7 +7,6 @@ import Vuelidate from "vuelidate";
 import VuePhoneNumberInput from "vue-phone-number-input";
 import PortalVue from "portal-vue";
 import "vue-phone-number-input/dist/vue-phone-number-input.css";
->>>>>>> b2d6ca6e035590b6822f21e5baace623bf4ce5eb
 
 Vue.use(PortalVue);
 Vue.use(Vuelidate);
