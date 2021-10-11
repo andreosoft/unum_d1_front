e59255633f7f400dfaf665828c01c429f1e9ef5d
Emir <dzhetybaev19emir@gmail.com>
2021-10-11 22:28:21 +0600

    group chat creating

--- src/components/chat/List.vue
+++ src/components/chat/List.vue
@@ -1,5 +1,50 @@
 <template>
   <div>
+    <v-btn
+      style="width: 100%;"
+      class="mb-3"
+      color="#f9f8ff"
+      @click="createGroupChatDialog = true"
+    >
+      {{ getDoctorTranslation("Create new group chat") }}
+    </v-btn>
+    <v-dialog v-model="createGroupChatDialog" max-width="500">
+      <v-card class="pa-5">
+        <form>
+          {{ getDoctorTranslation("Create new group chat") }}
+          <v-text-field
+            v-model="groupName"
+            :label="getDoctorTranslation('Your group title')"
+          ></v-text-field>
+          <v-select
+            v-model="selectedGroupMembers"
+            :items="getDoctors"
+            item-text="name"
+            item-value="user_id"
+            chips
+            multiple
+            :label="getDoctorTranslation('Your group members')"
+          >
+            <template v-slot:selection="data">
+              <v-chip
+                v-bind="data.attrs"
+                :input-value="data.selected"
+                close
+                @click:close="remove(data.item)"
+              >
+                {{ data.item.name }}
+              </v-chip>
+            </template>
+          </v-select>
+          <v-btn
+            :disabled="!selectedGroupMembers.length || !groupName.length"
+            @click="createGroup"
+          >
+            {{ getDoctorTranslation("Create group") }}
+          </v-btn>
+        </form>
+      </v-card>
+    </v-dialog>
     <v-text-field
       prepend-inner-icon="mdi-magnify"
       append-icon="mdi-account-plus"
@@ -14,11 +59,77 @@
 </template>
 
 <script>
+import { createNamespacedHelpers } from "vuex";
+const { mapGetters } = createNamespacedHelpers("lang");
+const { mapState: State_auth } = createNamespacedHelpers("auth");
+const { mapActions: Actions_alerts } = createNamespacedHelpers("alerts");
+const { mapActions: Actions_chats } = createNamespacedHelpers("chats");
+const { mapState: State_doctors } = createNamespacedHelpers("doctors");
 import ChatList from "./ChatList.vue";
 export default {
   name: "List",
   components: {
     ChatList,
+  },
+  data() {
+    return {
+      createGroupChatDialog: false,
+      selectedGroupMembers: [],
+      items: [
+        {
+          name: "Vuetify",
+          id: 1,
+        },
+        {
+          name: "Programming",
+          id: 2,
+        },
+        {
+          name: "Vue",
+          id: 3,
+        },
+        {
+          name: "Vuex",
+          id: 4,
+        },
+      ],
+      groupName: "",
+    };
+  },
+  computed: {
+    ...mapGetters(["getDoctorTranslation"]),
+    ...State_doctors(["doctors"]),
+    ...State_auth(["userProfile"]),
+    getDoctors() {
+      return (
+        this.doctors &&
+        this.doctors.filter((doc) => this.userProfile.id !== doc.user_id)
+      );
+    },
+  },
+  methods: {
+    ...Actions_alerts(["addAlert"]),
+    ...Actions_chats([
+      "createNewGroupChat",
+      "addUserToGroupChat",
+      "fetchChats",
+    ]),
+    async createGroup() {
+      const chat_id = await this.createNewGroupChat({ name: this.groupName });
+      for (let i = 0; i < this.selectedGroupMembers.length; i++) {
+        await this.addUserToGroupChat({
+          chat_id,
+          to_id: this.selectedGroupMembers[i],
+        });
+      }
+      this.fetchChats();
+      this.addAlert({ type: "success", text: "group has been created" });
+    },
+    remove(item) {
+      this.selectedGroupMembers = this.selectedGroupMembers.filter(
+        (id) => id !== item.user_id
+      );
+    },
   },
 };
 </script>
