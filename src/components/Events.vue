de50a944c115c42367d2397d2ce36fe8b52b5d45
Emir <dzhetybaev19emir@gmail.com>
2021-12-03 16:18:50 +0600

    Корректный вывод "Что будем делать"

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -22,7 +22,7 @@
                       <td class="table-title">
                         {{ getCommonTranslation("Event name") }}
                       </td>
-                      <td>{{ event.name }}</td>
+                      <td>{{ event.name === event.patient ? getDoctorTranslation('Patient reception'): event.name }}</td>
                     </tr>
                     <tr>
                       <td class="table-title">
@@ -82,8 +82,11 @@ export default {
     },
   },
   computed: {
-    ...mapGetters(["getCommonTranslation"]),
+    ...mapGetters(["getCommonTranslation", "getDoctorTranslation"]),
   },
+  mounted() {
+    console.log(this.events)
+  }
 };
 </script>
 
