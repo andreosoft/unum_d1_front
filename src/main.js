fcf59d0efd0087befcde3e2ec36fd2cfc1f44cef
Alexandr Kulik <kulik_alexandr@mail.ru>
2021-12-13 21:26:51 +0600

    новая карта поциента

--- src/main.js
+++ src/main.js
@@ -1,51 +1,51 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
+/** @format */
+
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
 import './libs/registerComponents';
 import './libs/translations';
-import Vuelidate from "vuelidate";
-import VuePhoneNumberInput from "vue-phone-number-input";
-import PortalVue from "portal-vue";
-import "vue-phone-number-input/dist/vue-phone-number-input.css";
-import VueChatScroll from "vue-chat-scroll";
-import { messaging, getToken } from './firebase'
-import { createNamespacedHelpers } from "vuex";
-const {
-  mapState: State_chats,
-  mapGetters: Getters_chats,
-  mapActions: Actions_chats,
-} = createNamespacedHelpers("chats");
-const { mapActions: Actions_alerts } = createNamespacedHelpers("alerts");
-const { mapState: State_auth } = createNamespacedHelpers("auth");
+import Vuelidate from 'vuelidate';
+import VuePhoneNumberInput from 'vue-phone-number-input';
+import PortalVue from 'portal-vue';
+import 'vue-phone-number-input/dist/vue-phone-number-input.css';
+import VueChatScroll from 'vue-chat-scroll';
+import { messaging, getToken } from './firebase';
+import { createNamespacedHelpers } from 'vuex';
+const { mapState: State_chats, mapGetters: Getters_chats, mapActions: Actions_chats } = createNamespacedHelpers('chats');
+const { mapActions: Actions_alerts } = createNamespacedHelpers('alerts');
+const { mapState: State_auth } = createNamespacedHelpers('auth');
+const { mapGetters: lang } = createNamespacedHelpers('lang');
 
 Vue.use(VueChatScroll);
 Vue.use(PortalVue);
 Vue.use(Vuelidate);
 Vue.config.productionTip = false;
-Vue.component("vue-phone-number-input", VuePhoneNumberInput);
+Vue.component('vue-phone-number-input', VuePhoneNumberInput);
 
 new Vue({
   data() {
     return {
-      msg: "message",
+      msg: 'message',
       ws: null,
       consiliumNotification: false,
       chatsNotification: false,
     };
   },
   computed: {
-    ...State_auth(["userProfile"]),
-    ...State_chats(["chats"]),
-    ...Getters_chats(["getSelectChatById"]),
+    ...State_auth(['userProfile']),
+    ...State_chats(['chats']),
+    ...Getters_chats(['getSelectChatById']),
+    ...lang(['getDoctorTranslation', 'getCommonTranslation']),
   },
   watch: {
     $route(val) {
-      if (val.name === "Chats") {
+      if (val.name === 'Chats') {
         this.chatsNotification = false;
       }
-      if (val.name === "Consilliums") {
+      if (val.name === 'Consilliums') {
         this.consiliumNotification = false;
       }
     },
@@ -54,31 +54,30 @@ new Vue({
       immediate: true,
       handler(val) {
         if (val.length) {
-          const selectedChat = this.getSelectChatById(
-            Number(this.$route.params.chatId)
-          );
+          const selectedChat = this.getSelectChatById(Number(this.$route.params.chatId));
           this.setSelectedChat(selectedChat);
         }
       },
     },
   },
   methods: {
-    ...Actions_chats(["addMessage", "fetchCurrentUserMessages", "fetchChats", "setSelectedChat"]),
-    ...Actions_alerts(["addAlert"]),
+    ...Actions_chats(['addMessage', 'fetchCurrentUserMessages', 'fetchChats', 'setSelectedChat']),
+    ...Actions_alerts(['addAlert']),
     webSocket() {
-      let usertoken = JSON.parse(localStorage.getItem("neomedy")).token;
-      let socketUrl = "wss://api.neomedy.com/messages";
-      if ("WebSocket" in window) {
+      let usertoken = JSON.parse(localStorage.getItem('neomedy')).token;
+      let socketUrl = 'wss://api.neomedy.com/messages';
+      if ('WebSocket' in window) {
         this.ws = new WebSocket(socketUrl);
         // const FCMToken = window.localStorage.getItem("FCM token")
         let timer;
         this.ws.onopen = () => {
-          this.ws.send(JSON.stringify({ e: "auth", d: usertoken }));
-          getToken(messaging, { vapidKey: "BLPcjvIYgCpYwdhZe-b9toT-BvwulE48yPwF2IbzEh0PzPoX3pU_KAurTKA76XOqtdrXP3eJAxssbgRH-Fgv9Vc" }).then((currentToken) => {
-            if (currentToken) {
+          this.ws.send(JSON.stringify({ e: 'auth', d: usertoken }));
+          getToken(messaging, { vapidKey: 'BLPcjvIYgCpYwdhZe-b9toT-BvwulE48yPwF2IbzEh0PzPoX3pU_KAurTKA76XOqtdrXP3eJAxssbgRH-Fgv9Vc' })
+            .then((currentToken) => {
+              if (currentToken) {
                 this.ws.send(
                   JSON.stringify({
-                    e: "register_firebase",
+                    e: 'register_firebase',
                     params: {
                       token: currentToken,
                     },
@@ -87,24 +86,25 @@ new Vue({
                 // window.localStorage.setItem('FCM token', currentToken)
                 // Send the token to your server and update the UI if necessary
                 // ...
-            } else {
+              } else {
                 // Show permission request UI
                 console.log('No registration token available. Request permission to generate one.');
                 // ...
-            }
-          }).catch((err) => {
+              }
+            })
+            .catch((err) => {
               console.log('An error occurred while retrieving token. ', err);
               // ...
-          });
+            });
           timer = setInterval(() => {
-            let d = { e: "h" };
+            let d = { e: 'h' };
             this.ws.send(JSON.stringify(d));
           }, 30000);
         };
         this.ws.onmessage = (evt) => {
           const parsedMessage = JSON.parse(evt.data);
           const currentChatId = this.$route.params.chatId;
-          if (parsedMessage.event && parsedMessage.event === "new_message") {
+          if (parsedMessage.event && parsedMessage.event === 'new_message') {
             parsedMessage.attachments = JSON.parse(parsedMessage.attachments);
             if (!currentChatId) {
               if (this.getChatType(parsedMessage.chat_id) !== 3) {
@@ -128,37 +128,31 @@ new Vue({
             }
             return;
           }
-          if (parsedMessage.event && parsedMessage.event === "new_chat") {
-            const ifCurrentUserMemberOfNewChat = parsedMessage.participants.findIndex(
-              (item) => item.user_id === this.userProfile.id
-            );
+          if (parsedMessage.event && parsedMessage.event === 'new_chat') {
+            const ifCurrentUserMemberOfNewChat = parsedMessage.participants.findIndex((item) => item.user_id === this.userProfile.id);
 
             if (ifCurrentUserMemberOfNewChat) {
               this.fetchChats();
             }
           }
           if (parsedMessage.event && parsedMessage.event === 'remove_user_from_chat') {
-            this.fetchChats()
+            this.fetchChats();
             if (Number(parsedMessage.chat_id) === Number(currentChatId)) {
-              this.$router.push({ name: 'Chats' })
+              this.$router.push({ name: 'Chats' });
             }
           }
         };
         this.ws.onclose = () => {
-          console.log("Connection is closed...");
+          console.log('Connection is closed...');
           clearInterval(timer);
           setTimeout(this.webSocket, 1000);
         };
         this.ws.onerror = (err) => {
-          console.error(
-            "Socket encountered error: ",
-            err.message,
-            "Closing socket"
-          );
+          console.error('Socket encountered error: ', err.message, 'Closing socket');
           this.ws.close();
         };
       } else {
-        console.log("WebSocket NOT supported by your Browser!");
+        console.log('WebSocket NOT supported by your Browser!');
       }
     },
     getChatType(chatId) {
@@ -178,4 +172,4 @@ new Vue({
   store,
   vuetify,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
