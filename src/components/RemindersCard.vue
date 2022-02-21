3155dedb81283c221bc945e9d2439ec730ca247f
k-alex-s <kulik_alexandr@mail.ru>
2022-02-21 23:13:29 +0600

    fix bugs

--- src/components/RemindersCard.vue
+++ src/components/RemindersCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pr-2" ref="">
     <v-list
-      two-line
+      two-line1
       :height="heightReminder"
       v-if="reminders && reminders.length"
     >
@@ -14,7 +14,7 @@
         :style="`background: ${item.color}`"
       >
         <template v-slot:default="{ active }">
-          <v-list-item-content>
+          <v-list-item-content class="py1-0">
             <v-list-item-title>
               {{ getEventName(item.sourceId) }}
             </v-list-item-title>
@@ -40,6 +40,8 @@
     <div v-else>
       <h4 class="mx-3">{{ $t("No reminders yet") }}</h4>
     </div>
+
+    <confirm-dialogue ref="confirmDialogue"></confirm-dialogue>
   </div>
 </template> 
 
@@ -64,9 +66,12 @@ export default {
   computed: {
     ...mapState({
       reminders: (state) => state.reminders.reminders,
+      userProfile: (state) => state.auth.userProfile,
       events: (state) => state.events.events,
     }),
+    //reminders() {      return this.$store.state.reminders.reminders;    },
     reminderSort() {
+      //console.log("reminderSort", this.reminders);
       let res = this.reminders.sort((a, b) => {
         let aa = _.min(a.duration.filter((el) => dayjs().isAfter(el)));
         let bb = _.min(b.duration.filter((el) => dayjs().isAfter(el)));
@@ -92,9 +97,11 @@ export default {
           this.$refs.listItem.clientHeight,
           this.reminders?.length
         );
-      let heightList = this.$parent.$refs.patientsList.clientHeight;
-      let heightBar = this.$parent.$parent.$refs.mainBar.$el.clientHeight;
-      let heightMain = this.$parent.$parent.$refs.mainLayout.clientHeight;
+      let heightList = this.$parent.$refs.patientsList?.clientHeight;
+      let heightBar = this.$parent.$parent.$refs.mainBar.$el?.clientHeight;
+      let heightMain = this.$parent.$parent.$refs.mainLayout
+        ? this.$parent.$parent.$refs.mainLayout?.clientHeight
+        : 0;
       let HeightListItem = this.$parent.$refs.patientsList.clientHeight;
       let heightReminder = heightMain - heightBar - heightList; //this.$parent.$refs.reminder.clientHeight;
       heightReminder =
@@ -103,13 +110,32 @@ export default {
           : heightReminder > 400
           ? 400
           : heightReminder;
-      if (!this.reminders || this.reminders?.length < 2) heightReminder = 90;
+      if (
+        !this.reminderSort ||
+        this.reminderSort?.filter((el) => {
+          if (dayjs().isBefore(el.date)) {
+            return true;
+          }
+          return false;
+        })?.length < 2
+      ) {
+        // console.log("ffff", this.reminderSort);
+        heightReminder = 90;
+      }
+      // console.log("height", heightReminder, window.innerHeight);
       return heightReminder > window.innerHeight * 0.4
         ? window.innerHeight * 0.4
         : heightReminder;
     },
   },
-  watch: {},
+  watch: {
+    userProfile(v) {
+      if (v) {
+        console.log("reminders/fetchReminders");
+        this.$store.dispatch("reminders/fetchReminders");
+      }
+    },
+  },
   created() {
     window.addEventListener("resize", this.matchHeight);
   },
@@ -118,10 +144,14 @@ export default {
     //this.matchHeight();
   },
   methods: {
+    minDuration(d) {
+      return _.min(d);
+    },
     reminderShow(item) {
+      return true;
       if (
         item.duration.filter((el) => {
-          if (dayjs().isAfter(el)) {
+          if (dayjs().isBefore(el)) {
             return true;
           }
           return false;
@@ -130,9 +160,40 @@ export default {
         return true;
       return false;
     },
-    clickReminder(e) {
-      e.duration.splice(0, 1);
-      this.$store.dispatch("reminders/updateReminder", e);
+    async clickReminder(e) {
+      let ok = false;
+      if (e.duration.length == 1 || 1) {
+        ok = await this.$refs.confirmDialogue.show({
+          title: this.$t("Delete reminder"),
+          message: [
+            e.message,
+            this.$t("Are you sure you want to delete this reminder?"),
+          ],
+          okButton: this.$t("Yes"),
+          cancelButton: this.$t("No"),
+        });
+      } else {
+        ok = true;
+      }
+      // If you throw an error, the method will terminate here unless you surround it wil try/catch
+      if (ok) {
+        console.log(
+          "duration",
+          e.duration.sort((a, b) => {
+            let r = 0;
+            if (a < b) {
+              r = -1;
+            }
+            if (a > b) {
+              r = 1;
+            }
+            return r;
+          })
+        );
+        e.duration.splice(0, 1);
+        this.$store.dispatch("reminders/updateReminder", e);
+      } else {
+      }
     },
     getEventName(id) {
       return this.events.find((el) => {
